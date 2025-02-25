7f94a1a0bb60b06f518aa4128342a722a0cd1085
Angelica <2233073055@correo.ler.uam.mx>
2025-02-25 13:02:10 -0600

    actualización uno

--- ejercicio_dos/script.js
+++ ejercicio_dos/script.js
@@ -1,7 +1,9 @@
-//variables
+// variables
 
-let animal1 ="gato";
-let tipo= "23";
+let ingrediente1 = "Crema";
+let costo = 40;
 
-//document.write(animal1);
-document.getElementById('prueba').innerHTML="Un gato en adopcion: "+animal1;
+//document.write(ingrediente1);
+document.getElementById('demo').innerHTML="Necesitamos de " + ingrediente1;
+document.getElementById('dem').innerHTML="El cual tiene un costo de: " + costo;
+document.getElementById('de').innerHTML="El precio del " + ingrediente1 + " es de: " + costo + ".";
