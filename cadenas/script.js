60d94c7261f15d07b941247fa40bc13e24d07a6b
Angelica <2233073055@correo.ler.uam.mx>
2025-03-14 08:58:06 -0600

    cadena

---
+++ cadenas/script.js
@@ -0,0 +1,9 @@
+let txt="metodo de cadena";
+let txt2 = txt.toUpperCase();
+let txt3 = txt.replace("cadena", "string");
+document.getElementById ("variable").innerHTML=txt
+document.getElementById ("variable2").innerHTML=txt2
+document.getElementById ("variable3").innerHTML=txt3
+//console.log(txt.length)
+//console.log(txt.at(10));
+console.log(txt.toUpperCase());
